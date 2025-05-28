e02ea3aec18a955f679796caa571a435e58540cc
Ben Cutler <ben.cutler@datarobot.com>
2025-05-28 14:30:31 -0400

    Fix whitespace and remove parens

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -10,12 +10,12 @@ package auth
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
-	"context"
-	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -31,7 +31,7 @@ var (
 func createAuthFileDirIfNotExists() error {
 	// TODO: we create a CLI config file here basically, so need to reflect that in the method names and structure
 	_, err := os.Stat(authFilePath)
-	if (err == nil) {
+	if err == nil {
 		// File exists, do nothing
 		return nil
 	}
@@ -58,7 +58,6 @@ func clearAuthFile() error {
 	return os.Truncate(authFilePath, 0)
 }
 
-
 func waitForAPIKeyCallback() string {
 	addr := "localhost:51164"
 	apiKeyChan := make(chan string)
@@ -77,8 +76,8 @@ func waitForAPIKeyCallback() string {
 
 	// Start the server in a goroutine
 	go func() {
-	    fmt.Println("Via this link : https://staging.datarobot.com/account/developer-tools?cliRedirect=true")
-		if (err := server.ListenAndServe(); err != http.ErrServerClosed) {
+		fmt.Println("Via this link : https://staging.datarobot.com/account/developer-tools?cliRedirect=true")
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
 			fmt.Printf("Server error: %v\n", err)
 		}
 	}()
@@ -93,8 +92,6 @@ func waitForAPIKeyCallback() string {
 
 	return apiKey
 }
-
-
 
 func LoginAction() error {
 	reader := bufio.NewReader(os.Stdin)
@@ -132,7 +129,7 @@ func LoginAction() error {
 		return fmt.Errorf("failed to create auth file: %w", err)
 	}
 
-    key:= waitForAPIKeyCallback();
+	key := waitForAPIKeyCallback()
 
 	if _, err := file.WriteString(strings.Replace(key, "\n", "", -1)); err != nil {
 		return err
