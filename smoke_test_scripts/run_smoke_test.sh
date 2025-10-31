943f43547aca4273f1cc6a0bc67007ceb0c84ac4
Chris Russell-Walker <c.russell.walker@gmail.com>
2025-10-31 12:56:09 -0400

    [CFX-3970] Smoke test templates setup

--- smoke_test_scripts/run_smoke_test.sh
+++ smoke_test_scripts/run_smoke_test.sh
@@ -18,11 +18,11 @@ dr completion bash > completion_bash.sh
 # Check if we have the file with expected __start_dr() function
 function_check=$(cat completion_bash.sh | grep __start_dr\()
 if [[ -n "$function_check" ]]; then
-  echo "Assertion passed: We have expected completion_bash.sh file."
+  echo "✅ Assertion passed: We have expected completion_bash.sh file."
   # Remove created bash file - especially helpful when running smoke tests locally
   rm completion_bash.sh
 else
-  echo "Assertion failed: We don't have expected completion_bash.sh file w/ expected function: __start_dr()."
+  echo "❌ Assertion failed: We don't have expected completion_bash.sh file w/ expected function: __start_dr()."
   # Print completion_bash.sh (if it exists) to aid in debugging if needed
   cat completion_bash.sh
   exit 1
@@ -37,10 +37,10 @@ expect ./smoke_test_scripts/expect_auth_setURL.exp $DATAROBOT_CLI_CONFIG
 # Check if we have the auth URL correctly set
 auth_endpoint_check=$(cat $DATAROBOT_CLI_CONFIG | grep endpoint | grep ${testing_url}/api/v2)
 if [[ -n "$auth_endpoint_check" ]]; then
-  echo "Assertion passed: We have expected expected 'endpoint' auth URL value in config."
+  echo "✅ Assertion passed: We have expected expected 'endpoint' auth URL value in config."
   echo "Value: $auth_endpoint_check"
 else
-  echo "Assertion failed: We don't have expected 'endpoint' auth URL value."
+  echo "❌ Assertion failed: We don't have expected 'endpoint' auth URL value."
   # Print ~/.config/datarobot/drconfig.yaml (if it exists) to aid in debugging if needed
   echo "${DATAROBOT_CLI_CONFIG} contents:"
   cat $DATAROBOT_CLI_CONFIG
@@ -60,14 +60,35 @@ expect ./smoke_test_scripts/expect_dotenv_setup.exp
 # Check if we have the value correctly set
 endpoint_check=$(cat .env | grep DATAROBOT_ENDPOINT=${testing_url}/api/v2)
 if [[ -n "$endpoint_check" ]]; then
-  echo "Assertion passed: We have expected DATAROBOT_ENDPOINT value (${testing_url}/api/v2) in created .env file."
+  echo "✅ Assertion passed: We have expected DATAROBOT_ENDPOINT value (${testing_url}/api/v2) in created .env file."
   echo "Value: $endpoint_check"
 else
-  echo "Assertion failed: We don't have expected DATAROBOT_ENDPOINT value in created .env file."
+  echo "❌ Assertion failed: We don't have expected DATAROBOT_ENDPOINT value in created .env file."
   # Print .env (if it exists) to aid in debugging if needed
   cat .env
   exit 1
 fi
 
-# TODO: Test this - likely may end up using another expect script
-# echo -ne 'q' | unbuffer -p dr templates setup
+# Test templates - Confirm expect script has cloned TTMDocs and that .env has expected value
+expect ./smoke_test_scripts/expect_templates_setup.exp
+testing_session_secret_key="TESTING_SESSION_SECRET_KEY"
+DIRECTORY="./talk-to-my-docs-agents"
+if [ -d "$DIRECTORY" ]; then
+  echo "✅ Directory ($DIRECTORY) exists."
+else
+  echo "❌ Directory ($DIRECTORY) does not exist."
+  exit 1
+fi
+cd $DIRECTORY
+session_secret_key_check=$(cat .env | grep SESSION_SECRET_KEY=${testing_session_secret_key})
+if [[ -n "$session_secret_key_check" ]]; then
+  echo "✅ Assertion passed: We have expected SESSION_SECRET_KEY in created .env file."
+else
+  echo "❌ Assertion failed: We don't have expected SESSION_SECRET_KEY value in created .env file."
+  # Print .env (if it exists) to aid in debugging if needed
+  cat .env
+  exit 1
+fi
+# Now delete directory to clean up
+cd ..
+rm -rf $DIRECTORY
