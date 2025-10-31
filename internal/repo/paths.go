9f87412201628ff36592ac46fe61a3467b6e75bf
AJ Alon <aj.alon@datarobot.com>
2025-10-31 14:21:11 -0700

    refactor

--- internal/repo/paths.go
+++ internal/repo/paths.go
@@ -1,6 +1,8 @@
-package internal
+package repo
 
 const (
+	// DataRobotRepoPath is the path to the DataRobot CLI folder relative to CWD
+	DataRobotRepoPath = ".datarobot/cli"
 	// QuickstartScriptPath is the path to the quickstart scripts directory relative to CWD
 	QuickstartScriptPath = ".datarobot/cli/bin"
 )
