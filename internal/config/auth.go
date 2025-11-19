8e0187593b466d319126e71fead167b21b645b54
Chris Russell-Walker <c.russell.walker@gmail.com>
2025-11-19 12:09:47 -0500

    [CFX-4097] Add User-Agent header to requests made to DR

--- internal/config/auth.go
+++ internal/config/auth.go
@@ -9,97 +9,12 @@
 package config
 
 import (
-	"errors"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
+	"github.com/charmbracelet/log"
 	"github.com/spf13/viper"
 )
-
-var ErrInvalidURL = errors.New("Invalid URL.")
-
-// SchemeHostOnly takes a URL like: https://app.datarobot.com/api/v2 and just
-// returns https://app.datarobot.com (no trailing slash)
-func SchemeHostOnly(longURL string) (string, error) {
-	parsedURL, err := url.Parse(longURL)
-	if err != nil {
-		return "", err
-	}
-
-	if parsedURL.Host == "" {
-		return "", ErrInvalidURL
-	}
-
-	parsedURL.Path, parsedURL.RawQuery, parsedURL.Fragment = "", "", ""
-
-	return parsedURL.String(), nil
-}
-
-func GetBaseURL() string {
-	if endpoint := viper.GetString(DataRobotURL); endpoint != "" {
-		if newURL, err := SchemeHostOnly(endpoint); err == nil {
-			return newURL
-		}
-	}
-
-	return ""
-}
-
-func GetEndpointURL(endpoint string) (string, error) {
-	baseURL := GetBaseURL()
-	if baseURL == "" {
-		return "", errors.New("Empty URL.")
-	}
-
-	return url.JoinPath(baseURL, endpoint)
-}
-
-func SaveURLToConfig(newURL string) error {
-	newURL, err := SchemeHostOnly(urlFromShortcut(newURL))
-	if err != nil {
-		return err
-	}
-
-	if err := CreateConfigFileDirIfNotExists(); err != nil {
-		return err
-	}
-
-	// Saves the URL to the config file with the path prefix
-	// Or as an empty string, if that's needed
-	if newURL == "" {
-		viper.Set(DataRobotURL, "")
-		viper.Set(DataRobotAPIKey, "")
-
-		_ = viper.WriteConfig()
-
-		return nil
-	}
-
-	viper.Set(DataRobotURL, newURL+"/api/v2")
-
-	_ = viper.WriteConfig()
-
-	return nil
-}
-
-func urlFromShortcut(selectedOption string) string {
-	selected := strings.TrimSpace(selectedOption)
-
-	switch selected {
-	case "":
-		return ""
-	case "1":
-		return "https://app.datarobot.com"
-	case "2":
-		return "https://app.eu.datarobot.com"
-	case "3":
-		return "https://app.jp.datarobot.com"
-	default:
-		return selected
-	}
-}
 
 // VerifyToken verifies if the datarobot host + api key pair correspond to a valid pair.
 func VerifyToken(datarobotHost, token string) (bool, error) {
@@ -110,6 +25,9 @@ func VerifyToken(datarobotHost, token string) (bool, error) {
 
 	bearer := "Bearer " + token
 	req.Header.Add("Authorization", bearer)
+	req.Header.Add("User-Agent", GetUserAgentHeader())
+
+	log.Debug("Request Info: \n" + RedactedReqInfo(req))
 
 	client := &http.Client{}
 
