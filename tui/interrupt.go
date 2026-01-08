e8021b16fe13b1652b98e45be1f6e2a5eb49a1eb
AJ Alon <aj.alon@datarobot.com>
2026-01-08 12:10:26 -0800

    update Go files

--- tui/interrupt.go
+++ tui/interrupt.go
@@ -1,11 +1,3 @@
-// Copyright 2025 DataRobot, Inc. and its affiliates.
-// All rights reserved.
-// DataRobot, Inc. Confidential.
-// This is unpublished proprietary source code of DataRobot, Inc.
-// and its affiliates.
-// The copyright notice above does not evidence any actual or intended
-// publication of such source code.
-
 package tui
 
 import tea "github.com/charmbracelet/bubbletea"
