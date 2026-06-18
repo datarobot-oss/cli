db1df73538cb8a908f1a7ae982f6efee94c7ef59
AJ Alon <aj.alon@datarobot.com>
2026-06-18 13:18:30 -0700

    feat(component): upgrade list to lipgloss/table and add JSON output

--- cmd/component/list/cmd.go
+++ cmd/component/list/cmd.go
@@ -17,27 +17,77 @@ package list
 import (
 	"fmt"
 	"os"
-	"text/tabwriter"
 
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 	"github.com/datarobot/cli/internal/copier"
+	"github.com/datarobot/cli/internal/outputformat"
+	"github.com/datarobot/cli/tui"
 	"github.com/spf13/cobra"
 )
 
-func RunE(_ *cobra.Command, _ []string) error {
+// ComponentOutput is the JSON representation of an installed component for --output-format json.
+type ComponentOutput struct {
+	Name string `json:"name"`
+	File string `json:"file"`
+	Repo string `json:"repo"`
+}
+
+func RunE(cmd *cobra.Command, _ []string) error {
 	answers, err := copier.AnswersFromPath(".", false)
 	if err != nil {
 		return err
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-
-	fmt.Fprintf(w, "Component name\tAnswers file\tRepository\n")
-
-	for _, answer := range answers {
-		fmt.Fprintf(w, "%s\t%s\t%s\n", answer.ComponentDetails.Name, answer.FileName, answer.Repo)
+	format := outputformat.GetFormat(cmd)
+	if format == outputformat.OutputFormatJSON {
+		outputs := make([]ComponentOutput, len(answers))
+		for i, a := range answers {
+			outputs[i] = ComponentOutput{
+				Name: a.ComponentDetails.Name,
+				File: a.FileName,
+				Repo: a.Repo,
+			}
+		}
+		return outputformat.PrintJSONEnvelope(os.Stdout, "components", outputs)
 	}
 
-	w.Flush()
+	if len(answers) == 0 {
+		fmt.Println("No components installed.")
+		return nil
+	}
+
+	nameStyle := tui.BaseTextStyle.
+		Foreground(tui.GetAdaptiveColor(tui.DrPurple, tui.DrPurpleDark)).
+		Padding(0, 1)
+
+	fileStyle := tui.DimStyle.
+		Padding(0, 1)
+
+	repoStyle := tui.BaseTextStyle.
+		Foreground(tui.GetAdaptiveColor(tui.DrPurpleLight, tui.DrPurpleDarkLight)).
+		Padding(0, 1)
+
+	t := table.New().
+		Border(lipgloss.RoundedBorder()).
+		BorderStyle(tui.TableBorderStyle).
+		StyleFunc(func(_, col int) lipgloss.Style {
+			switch col {
+			case 0:
+				return nameStyle
+			case 1:
+				return fileStyle
+			default:
+				return repoStyle
+			}
+		}).
+		Headers("NAME", "FILE", "REPO")
+
+	for _, a := range answers {
+		t.Row(a.ComponentDetails.Name, a.FileName, a.Repo)
+	}
+
+	_, _ = fmt.Fprintln(os.Stdout, t.Render())
 
 	return nil
 }
